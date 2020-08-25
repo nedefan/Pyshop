13bbca12f323892f5890ef513c2e208c51a1ec02
Александр Нельде <aleksandrnelde@iMac-Aleksandr.local>
2020-08-25 19:29:26 +0600

    Class Product, Offer

--- products/views.py
+++ products/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse ('Hello world')
+    return HttpResponse('Hello world')
 
 
 def new(request):
-    return HttpResponse ('New product')
+    return HttpResponse('New products')
